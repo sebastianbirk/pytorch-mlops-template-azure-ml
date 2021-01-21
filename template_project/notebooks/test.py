4899875d3f71b09c1284c134435687a9013fe566
Sebastian Birk <sebastian.birk@outlook.com>
2021-01-21 06:54:43 +0000

    Modify environment_creation notebook and add aml_snippets

--- template_project/notebooks/test.py
+++
@@ -1,6 +0,0 @@
-from azureml.core import Environment, Workspace
-ws = Workspace.from_config() 
-myenv = Environment(name="myenv")
-
-registered_env = myenv.register(ws)
-registered_env.build_local(ws)
