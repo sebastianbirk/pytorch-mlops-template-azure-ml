faa8501546fc7b62fa056ff2ee72ad212000f74d
Sebastian Birk <sebastian.birk@outlook.com>
2022-08-28 19:39:21 +0200

    Update container_registry.tf

--- infrastructure/container_registry.tf
+++ infrastructure/container_registry.tf
@@ -2,8 +2,8 @@
 
 resource "azurerm_container_registry" "mlops_template_cr" {
   name                = "mlopstemplatecr${lower(random_id.suffix.hex)}"
-  resource_group_name = azurerm_resource_group.mlops_template_rg.location
-  location            = azurerm_resource_group.mlops_template_rg.name
+  resource_group_name = azurerm_resource_group.mlops_template_rg.name
+  location            = azurerm_resource_group.mlops_template_rg.location
   sku                 = "Premium"
   admin_enabled       = true
 }
