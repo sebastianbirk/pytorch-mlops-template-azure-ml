f88c6cbd6422056296315f00665dc19752bb963c
Sebastian Birk <sebastian.birk@outlook.com>
2022-08-29 10:39:06 +0200

    Update web_app.tf

--- infrastructure/web_app.tf
+++ infrastructure/web_app.tf
@@ -13,6 +13,21 @@ resource "azurerm_linux_web_app" "mlops_template_wa" {
   resource_group_name = azurerm_resource_group.mlops_template_rg.name
   location            = azurerm_resource_group.mlops_template_rg.location
   service_plan_id     = azurerm_service_plan.mlops_template_sp.id
+  
+  identity {
+    type = "SystemAssigned"
+  }
+  
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_PASSWORD" = ""
+    "DOCKER_REGISTRY_SERVER_URL" = "https://mlopstemplatecr${lower(random_id.suffix.hex)}.azurecr.io"
+    "DOCKER_REGISTRY_SERVER_USERNAME" = "mlopstemplatecr${lower(random_id.suffix.hex)}"
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
 
-  site_config {}
+  site_config {
+    application_stack {
+      docker_image     = ""
+      docker_image_tag = ""
+    }
 }
